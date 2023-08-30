e6b8b85d5e8b48c380dfca1ff4fb70414bb0f2bd
nikhilgulwade <nikhil.gulwade@gmail.com>
2023-08-30 10:48:09 +0530

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -47,7 +47,7 @@ def main():
 
         with col4:
             st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]["summary"])
+            st.write(podcast_info["podcast_guest"][3])
 
         #with col4:
           #st.subheader("Guest Summary")
@@ -96,11 +96,11 @@ def main():
 
         with col3:
             st.subheader("Podcast Guest")
-            st.write(podcast_info["podcast_guest"]["name"])
+            st.write(podcast_info["podcast_guest"][0])
 
         with col4:
             st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['summary'])
+            st.write(podcast_info["podcast_guest"][3])
 
         #with col4:
           #st.subheader("Guest Summary")
