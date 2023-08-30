c31082e037abfa2d3bc705bf62ddfd97b89c4ee2
nikhilgulwade <nikhil.gulwade@gmail.com>
2023-08-30 11:07:01 +0530

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -42,8 +42,22 @@ def main():
         #col3, col4 = st.columns([3, 7])
 
         #with col3:
-            st.subheader("Podcast Guest")
-            st.write(podcast_info["podcast_guest"][0:])
+        st.subheader("Podcast Guest")
+        guest_info = podcast_info['podcast_guest']
+        if isinstance(guest_info, dict):
+          guest_name = guest_info.get('name', 'Name did not find!')
+        else:
+          guest_name = guest_info
+        st.write(guest_name)
+
+
+        st.subheader("Podcast Guest Details")
+        guest_info = podcast_info['podcast_guest']
+        if isinstance(guest_info, dict):
+          guest_summary = guest_info.get('summary', 'The Name you are searching does not match with any pages on Wikipedia!')
+        else:
+          guest_summary = guest_info
+        st.write(guest_summary)
 
         #with col4:
             #st.subheader("Podcast Guest Details")
@@ -95,8 +109,22 @@ def main():
         #col3, col4 = st.columns([3, 7])
 
         #with col3:
-            st.subheader("Podcast Guest")
-            st.write(podcast_info["podcast_guest"][0:])
+        st.subheader("Podcast Guest")
+        guest_info = podcast_info['podcast_guest']
+        if isinstance(guest_info, dict):
+          guest_name = guest_info.get('name', 'Name did not find!')
+        else:
+          guest_name = guest_info
+        st.write(guest_name)
+
+
+        st.subheader("Podcast Guest Details")
+        guest_info = podcast_info['podcast_guest']
+        if isinstance(guest_info, dict):
+          guest_summary = guest_info.get('summary', 'The Name you are searching does not match with any pages on Wikipedia!')
+        else:
+          guest_summary = guest_info
+        st.write(guest_summary)
 
         #with col4:
             #st.subheader("Podcast Guest Details")
